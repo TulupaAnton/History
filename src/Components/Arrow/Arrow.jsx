bd7207e7bfbe99cd15999ebc29cec5e263d82afa
foxstudent107933 <tulupaanton11@gmail.com>
2025-07-27 00:07:41 +0300

    Fix image path for Vercel

--- src/Components/Arrow/Arrow.jsx
+++ src/Components/Arrow/Arrow.jsx
@@ -1,7 +1,7 @@
 export function Arrow () {
   return (
     <img
-      src='./src/public/arrow.png'
+      src='/arrow.png'
       alt='Историческая стрелка вниз'
       className='w-12 h-auto opacity-90'
     />
--- src/Components/BlocksOfHistory/Oleg/Oleg.jsx
+++ src/Components/BlocksOfHistory/Oleg/Oleg.jsx
@@ -6,7 +6,7 @@ export function Oleg () {
     <div className='flex flex-col items-center justify-start  bg-yellow-50 pt-10'>
       <div className='max-w-xs border-4 w-60 border-yellow-800 rounded-xl overflow-hidden shadow-md hover:shadow-lg transform hover:-translate-y-1 transition duration-300 bg-yellow-50 bg-[url("/src/assets/pergament-texture.jpg")] bg-cover'>
         <img
-          src='./src/public/oleg.jpg'
+          src='/oleg.jpg'
           alt='Oleg'
           className='w-full object-cover h-48 border-b-4 border-yellow-800'
         />
@@ -28,7 +28,7 @@ export function Oleg () {
         <div className='text-center text-red-800 font-semibold italic mt-2'>
           Гадюка виползає з юлюбленого коня та вбиває Олега
           <img
-            src='./src/public/snake.png'
+            src='/snake.png'
             alt='Змея'
             className='w-6 h-6 inline-block mr-2'
           />
